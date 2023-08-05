cde3a1e8c546451a5a834d91cba6aa31907d3ffa
cyber-lcy <134449327+cyber-lcy@users.noreply.github.com>
2023-08-05 15:31:40 +0200

    added further gamelogic in playRound()

--- main.js
+++ main.js
@@ -33,11 +33,26 @@ function getComputerChoice () {
 };
 
 function playRound () {
-    if (playerSelection !== computerSelection) {
-        console.log("NOT EQUAL!");
+    if (playerSelection === "knight" && computerSelection === "archer" ||
+    playerSelection === "knight" && computerSelection === "sorcerer") {
+        console.log(`Your ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} has slayn the enemies ${computerSelection}`);
+    } else if (playerSelection === "archer" && computerSelection === "sorcerer" ||
+    playerSelection === "archer" && computerSelection === "dragon") {
+        console.log(`${playerSelection} shoots down ${computerSelection}`);
+    } else if (playerSelection === "sorcerer" && computerSelection === "dragon" ||
+    playerSelection === "sorcerer" && computerSelection === "golem") {
+        console.log(`Your ${playerSelection} pulverized the enemies ${computerSelection}!`);
+    } else if ( playerSelection === "dragon" && computerSelection === "golem" ||
+    playerSelection === "dragon" && computerSelection === "knight") {
+        console.log(`Your mighty ${playerSelection} burned the enemies ${computerSelection} to ashes!`);
+    } else if (playerSelection === "golem" && computerSelection === "knight" ||
+    playerSelection === "golem" && computerSelection === "archer") {
+        console.log(`Your ${playerSelection} has crushed your enemies ${computerSelection} to dust!`);
+    } else if (playerSelection === computerSelection) {
+        console.log(`Two of the same kind never fight each other, its a tie!`);
     } else {
-        console.log("EQUAL!");
-    };
+        console.log(`Enemies ${computerSelection} erased your ${playerSelection}...`);
+    }
     console.log(playerSelection);
     console.log(computerSelection);
 };
